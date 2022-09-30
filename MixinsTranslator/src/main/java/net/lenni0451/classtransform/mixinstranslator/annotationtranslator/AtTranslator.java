78460413d80a8c882e0a2134796f4aac30f29c1b
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2022-09-30 19:14:32 +0200

    Added constant target translator

--- MixinsTranslator/src/main/java/net/lenni0451/classtransform/mixinstranslator/annotationtranslator/AtTranslator.java
+++ MixinsTranslator/src/main/java/net/lenni0451/classtransform/mixinstranslator/annotationtranslator/AtTranslator.java
@@ -14,6 +14,20 @@ public class AtTranslator implements IAnnotationTranslator {
     public void translate(Map<String, IAnnotationTranslator> translators, AnnotationNode annotation) {
         annotation.desc = Type.getDescriptor(CTarget.class);
         Map<String, Object> values = AnnotationParser.listToMap(annotation.values);
+        if (values.containsKey("value") && values.containsKey("target")) {
+            String value = (String) values.get("value");
+            String target = (String) values.get("target");
+            if (value.equalsIgnoreCase("CONSTANT")) {
+                if (target.startsWith("nullValue")) target = "null";
+                else if (target.startsWith("intValue=")) target = "int " + target.substring(9);
+                else if (target.startsWith("floatValue=")) target = "float " + target.substring(11);
+                else if (target.startsWith("longValue=")) target = "long " + target.substring(10);
+                else if (target.startsWith("doubleValue=")) target = "double " + target.substring(12);
+                else if (target.startsWith("stringValue=")) target = "string " + target.substring(12);
+                else if (target.startsWith("classValue=")) target = "type " + target.substring(11);
+                values.put("target", target);
+            }
+        }
         if (values.containsKey("shift")) {
             String[] shift = (String[]) values.get("shift");
             shift[0] = Type.getDescriptor(CTarget.Shift.class);
