8bb5b1cba61e6bbf69ea7851bd9ea5ed869ddadc
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-09-03 15:54:04 +0200

    Make inner class accessible fields and method public

--- src/main/java/net/lenni0451/classtransform/transformer/impl/general/InnerClassGeneralHandler.java
+++ src/main/java/net/lenni0451/classtransform/transformer/impl/general/InnerClassGeneralHandler.java
@@ -1,8 +1,11 @@
 package net.lenni0451.classtransform.transformer.impl.general;
 
 import net.lenni0451.classtransform.TransformerManager;
+import net.lenni0451.classtransform.annotations.CShadow;
 import net.lenni0451.classtransform.transformer.AnnotationHandler;
 import net.lenni0451.classtransform.utils.ASMUtils;
+import net.lenni0451.classtransform.utils.annotations.AnnotationParser;
+import net.lenni0451.classtransform.utils.annotations.AnnotationUtils;
 import net.lenni0451.classtransform.utils.mappings.MapRemapper;
 import net.lenni0451.classtransform.utils.mappings.Remapper;
 import org.objectweb.asm.Opcodes;
@@ -12,6 +15,7 @@ import org.objectweb.asm.tree.InnerClassNode;
 import org.objectweb.asm.tree.MethodNode;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Map;
 
 import static net.lenni0451.classtransform.utils.ASMUtils.dot;
 
@@ -31,8 +35,8 @@ public class InnerClassGeneralHandler extends AnnotationHandler {
             hasInnerClasses = true;
 
             transformerManager.addRawTransformer(dot(innerClass.name), (tm, transformedClass) -> {
-                for (MethodNode method : transformedClass.methods) method.access = ASMUtils.setAccess(method.access, Opcodes.ACC_PUBLIC);
                 for (FieldNode field : transformedClass.fields) field.access = ASMUtils.setAccess(field.access, Opcodes.ACC_PUBLIC);
+                for (MethodNode method : transformedClass.methods) method.access = ASMUtils.setAccess(method.access, Opcodes.ACC_PUBLIC);
                 transformedClass.access = ASMUtils.setAccess(transformedClass.access, Opcodes.ACC_PUBLIC);
                 transformedClass.outerClass = null;
 
@@ -43,11 +47,32 @@ public class InnerClassGeneralHandler extends AnnotationHandler {
             });
         }
         if (hasInnerClasses) {
-            for (MethodNode method : transformer.methods) {
-                if ((method.access & Opcodes.ACC_SYNTHETIC) != 0) {
-                    method.access = ASMUtils.setAccess(method.access, Opcodes.ACC_PUBLIC);
-                    method.access &= ~Opcodes.ACC_BRIDGE;
-                }
+            this.makeFieldsPublic(transformer);
+            this.makeMethodsPublic(transformer);
+        }
+    }
+
+    private void makeFieldsPublic(final ClassNode transformer) {
+        for (FieldNode field : transformer.fields) {
+            field.access = ASMUtils.setAccess(field.access, Opcodes.ACC_PUBLIC);
+            AnnotationUtils.findInvisibleAnnotation(field, CShadow.class).ifPresent(annotation -> {
+                Map<String, Object> values = AnnotationParser.listToMap(annotation.values);
+                values.put("makePublic", true);
+                annotation.values = AnnotationParser.mapToList(values);
+            });
+        }
+    }
+
+    private void makeMethodsPublic(final ClassNode transformer) {
+        for (MethodNode method : transformer.methods) {
+            if ((method.access & Opcodes.ACC_SYNTHETIC) != 0) {
+                method.access = ASMUtils.setAccess(method.access, Opcodes.ACC_PUBLIC);
+                method.access &= ~Opcodes.ACC_BRIDGE;
+                AnnotationUtils.findInvisibleAnnotation(method, CShadow.class).ifPresent(annotation -> {
+                    Map<String, Object> values = AnnotationParser.listToMap(annotation.values);
+                    values.put("makePublic", true);
+                    annotation.values = AnnotationParser.mapToList(values);
+                });
             }
         }
     }
