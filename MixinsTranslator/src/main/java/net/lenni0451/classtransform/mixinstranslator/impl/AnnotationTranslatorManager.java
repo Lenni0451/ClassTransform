19cea811540c8633dba5c5fd29a10ad039ab8227
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-12-01 19:31:24 +0100

    Removed empty translators

--- MixinsTranslator/src/main/java/net/lenni0451/classtransform/mixinstranslator/impl/AnnotationTranslatorManager.java
+++ MixinsTranslator/src/main/java/net/lenni0451/classtransform/mixinstranslator/impl/AnnotationTranslatorManager.java
@@ -2,6 +2,9 @@ package net.lenni0451.classtransform.mixinstranslator.impl;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
+import net.lenni0451.classtransform.annotations.CShadow;
+import net.lenni0451.classtransform.annotations.CShared;
+import net.lenni0451.classtransform.annotations.injection.COverride;
 import org.objectweb.asm.Type;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -23,12 +26,16 @@ public class AnnotationTranslatorManager {
         register(Inject.class, new InjectTranslator());
         register(Redirect.class, new RedirectTranslator());
         register(ModifyConstant.class, new ModifyConstantTranslator());
-        register(Overwrite.class, new OverwriteTranslator());
+        register(Overwrite.class, COverride.class);
         register(At.class, new AtTranslator());
-        register(Shadow.class, new ShadowTranslator());
+        register(Shadow.class, CShadow.class);
         register(Slice.class, new SliceTranslator());
-        register(Share.class, new ShareTranslator());
+        register(Share.class, CShared.class);
         register(Local.class, new LocalTranslator());
+    }
+
+    private static void register(final Class<? extends Annotation> from, final Class<? extends Annotation> to) {
+        register(from, annotation -> annotation.desc = Type.getDescriptor(to));
     }
 
     private static void register(final Class<? extends Annotation> clazz, final IAnnotationTranslator translator) {
