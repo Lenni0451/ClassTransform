08e1c9b66c909f3a0b4eef416b20e71bc82ad459
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-12-07 21:17:48 +0100

    Rewrote javadoc of CWrapCatch

--- src/main/java/net/lenni0451/classtransform/annotations/injection/CWrapCatch.java
+++ src/main/java/net/lenni0451/classtransform/annotations/injection/CWrapCatch.java
@@ -11,10 +11,35 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Wrap an entire method or a single instruction in a try-catch block.<br>
- * The transformer method must have the same return type as the target method.<br>
- * The transformer method requires an object extending {@link Throwable} as the only parameter.<br>
- * The parameter type is the type of the exception caught by the try-catch block.
+ * Wrap an entire method or a single method call in a try-catch block.<br>
+ * The transformer method must have the caught exception as the only parameter.<br>
+ * <p>When wrapping an entire method:</p>
+ * <ul>
+ *     <li>The transformer method must have the same return type as the wrapped method.</li>
+ * </ul>
+ * <p>When wrapping a method call instruction:</p>
+ * <ul>
+ *     <li>The transformer method must have the same return type as the wrapped method.</li>
+ * </ul>
+ * The transformer method has to be static if the target method is static.<br>
+ * <br>
+ * Wrapping an entire method:<br>
+ * <pre>
+ * &#64;CWrapCatch()
+ * public String catchException(Exception e) {
+ *      //Rethrow the exception or return a default value
+ * }
+ * </pre>
+ * Wrapping a method call instruction:<br>
+ * <pre>
+ * &#64;CWrapCatch(target = "...")
+ * public String catchException(Exception e) {
+ *      //Rethrow the exception or return a default value
+ * }
+ * </pre>
+ * If your target has to be chosen more precisely you can use a {@link #slice()} to narrow down the search.
+ *
+ * @see CSlice
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
@@ -23,7 +48,7 @@ public @interface CWrapCatch {
     /**
      * The method name and descriptor to inject into.<br>
      * This supports multiple targets and wildcards.<br>
-     * e.g. {@code print(Ljava/lang/String;)V}
+     * e.g. {@code print(Ljava/lang/String;)V} or {@code print*}
      *
      * @return The method name and descriptor
      */
@@ -31,8 +56,7 @@ public @interface CWrapCatch {
     String[] value();
 
     /**
-     * The method owner, name and descriptor of the target instruction to wrap.<br>
-     * Only method calls are supported.<br>
+     * The method owner, name and descriptor of the target method call instruction to wrap.<br>
      * If this is not specified the entire method will be wrapped.<br>
      * e.g. {@code Ljava/io/InputStream;close()V}
      *
@@ -42,7 +66,7 @@ public @interface CWrapCatch {
     String target() default "";
 
     /**
-     * The ordinal of the target instruction.<br>
+     * The ordinal of target method call instruction.<br>
      * Ignored if the entire method should be wrapped.
      *
      * @return The ordinal
@@ -50,7 +74,7 @@ public @interface CWrapCatch {
     int ordinal() default -1;
 
     /**
-     * The slice of instructions to search for the target.<br>
+     * The slice to narrow down the search for the target.<br>
      * Ignored if the entire method should be wrapped.
      *
      * @return The slice
