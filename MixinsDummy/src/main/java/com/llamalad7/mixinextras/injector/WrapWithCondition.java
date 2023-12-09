292bfd913536d49d7deb622c76ad8c1b311c3c7c
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-12-09 17:44:14 +0100

    Added WrapWithCondition mixins translator

---
+++ MixinsDummy/src/main/java/com/llamalad7/mixinextras/injector/WrapWithCondition.java
@@ -0,0 +1,33 @@
+package com.llamalad7.mixinextras.injector;
+
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WrapWithCondition {
+
+    String[] method();
+
+    /**
+     * An array of targets is not supported. Only the first target will be used.<br>
+     * An exception will be thrown during the translation process if more than one slice is specified.<br>
+     * <b>The only supported targets are {@code INVOKE} and {@code FIELD}.</b>
+     */
+    At[] at();
+
+    /**
+     * An array of slices is not supported. Only the first slice will be used.<br>
+     * An exception will be thrown during the translation process if more than one slice is specified.
+     */
+    Slice[] slice() default {};
+
+    @Deprecated
+    boolean remap() default true;
+
+}
